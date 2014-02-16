fa4fbb5ed07ed28ff1059a943f26a0e17c1f8c7b
Junfeng Dai <ryandjf@hotmail.com>
2014-02-16 12:13:37 +0800

    add sublime settings and shell script to set up ubuntu

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -10,3 +10,21 @@ mkdir -p ~/Src
 mkdir -p ~/Tools/idea
 mkdir -p ~/Donwloads
 
+wget -P ~/Downloads http://services.gradle.org/distributions/gradle-1.11-bin.zip
+unzip ~/Downloads/gradle-1.11-bin.zip -d ~/Tools
+echo 'export GRADLE_HOME="~/Tools/gradle-1.11"' >> ~/.bashrc
+echo 'export PATH="$GRADLE_HOME/bin:$PATH"' >> ~/.bashrc
+
+
+wget -P ~/Downloads http://download-ln.jetbrains.com/idea/ideaIC-13.0.2.tar.gz
+tar xvzf ~/Downloads/ideaIC-13.0.2.tar.gz -C ~/Tools/idea --strip-components 1
+echo 'export IDEA_HOME="~/Tools/idea"' >> ~/.bashrc
+echo 'export PATH="$IDEA_HOME/bin:$PATH"' >> ~/.bashrc
+
+git clone git@github.com:ryandjf/ansible-workshop.git ~/Src/ansible-workshop
+git clone git@github.com:ansible/ansible.git ~/Src/ansible
+sudo apt-get install -y python-setuptools
+sudo apt-get install -y python-pip
+sudo easy_install pip
+sudo pip install paramiko PyYAML jinja2 httplib2
+sudo apt-get install -y python-boto
