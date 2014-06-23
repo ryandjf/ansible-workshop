0e5d45ca22c6e44edea3d111404e2e5a8a1f2497
Junfeng Dai <ryandjf@hotmail.com>
2014-06-23 09:31:41 +0800

    update sh

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -3,6 +3,7 @@
 sudo apt-get install -y vim git wget curl unzip tar
 
 ssh-keygen -q -N '' ~/.ssh/id_rsa
+
 git config --global user.name "Junfeng Dai"
 git config --global user.email "ryandjf@hotmail.com"
 
